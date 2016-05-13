5657c2f079f2876ac191160d6fe618c2950f6b6f
SeregaKonon <sskononovss@gmail.com>
2016-05-13 15:36:28 +0300

    Revert "ready"
    This reverts commit 90cd1723dfa5f05a63f38a4eaf0be557a881c32c.

--- FirstEFApp/FirstEFApp/DAL/SomeTable.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstEFApp.DAL
-{
-    public class SomeTable
-    {
-        public int ID { get; set; }
-        public string SomeName { get; set; }
-        public double SomeDouble { get; set; }
-    }
-}
