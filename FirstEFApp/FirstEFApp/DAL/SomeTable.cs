90cd1723dfa5f05a63f38a4eaf0be557a881c32c
SeregaKonon <sskononovss@gmail.com>
2016-05-13 15:35:42 +0300

    ready

---
+++ FirstEFApp/FirstEFApp/DAL/SomeTable.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstEFApp.DAL
+{
+    public class SomeTable
+    {
+        public int ID { get; set; }
+        public string SomeName { get; set; }
+        public double SomeDouble { get; set; }
+    }
+}
---
+++ FirstEFApp/FirstEFApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using FirstEFApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstEFApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (SomeTableContext db = new SomeTableContext())
+            {
+                List<SomeTable> st = new List<SomeTable>();
+                for (int i = 0; i < 10; i++)
+                {
+                    st.Add(new SomeTable { SomeName = "Name_" + i.ToString(), SomeDouble = i/0.123 });
+                }
+                st.ForEach(s => db.SomeTables.Add(s));
+                db.SaveChanges();
+
+
+                //what was loaded in table?
+                var res = db.SomeTables;
+                foreach (var r in res)
+                {
+                    Console.WriteLine("id={0} \t name={1} \t double={2}",r.ID, r.SomeName, r.SomeDouble);
+                }
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
